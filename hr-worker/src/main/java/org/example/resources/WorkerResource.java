57cdf43b80a7400b5e7492920598e19d39354ad0
aronque <vitorsiwerskiaronque@gmail.com>
2025-01-01 11:11:07 -0300

    Adding Ribbon lib for load balance

--- hr-worker/src/main/java/org/example/resources/WorkerResource.java
+++ hr-worker/src/main/java/org/example/resources/WorkerResource.java
@@ -2,7 +2,10 @@ package org.example.resources;
 
 import org.example.entities.Worker;
 import org.example.repositories.WorkerRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +18,11 @@ import java.util.List;
 @RequestMapping(value = "/workers")
 public class WorkerResource {
 
+    private static Logger LOGGER = LoggerFactory.getLogger(WorkerResource.class);
+
+    @Autowired
+    private Environment env;
+
     @Autowired
     private WorkerRepository repository;
 
@@ -26,6 +34,9 @@ public class WorkerResource {
 
     @GetMapping("/{id}")
     public ResponseEntity<Worker> findById(@PathVariable Long id){
+
+        LOGGER.info("SERVERPORT: " + env.getProperty("local.server.port"));
+
         Worker result = repository.findById(id).get();
         return ResponseEntity.ok().body(result);
     }
