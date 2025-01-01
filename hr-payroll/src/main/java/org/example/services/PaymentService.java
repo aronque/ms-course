da896016f9af096b0ccfe1811d6a240205a7c485
aronque <vitorsiwerskiaronque@gmail.com>
2025-01-01 10:31:00 -0300

    RestTemplate implementation for apps communication

--- hr-payroll/src/main/java/org/example/services/PaymentService.java
+++ hr-payroll/src/main/java/org/example/services/PaymentService.java
@@ -1,12 +1,30 @@
 package org.example.services;
 
 import org.example.entities.Payment;
+import org.example.entities.Worker;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class PaymentService {
 
+    @Value("${hr-worker.host}")
+    private String workerHost;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
     public Payment getPayment(long workerId, int days) {
-        return new Payment("Teste1", 49000.0, days);
+        Map<String, String> paramMap = new HashMap();
+        paramMap.put("id", String.valueOf(workerId));
+
+        Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, paramMap);
+
+        return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
 }
